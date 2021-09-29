6d39960a36412f98ff41500e53eb4ac847b84fe8
pi31416chan <89824274+pi31416chan@users.noreply.github.com>
2021-09-29 09:37:07 +0800

    Add files via upload

---
+++ Self Learning/numpy_exercises.py
@@ -0,0 +1,31 @@
+import numpy as np
+
+
+
+# Exercises
+'''
+14. Write a NumPy program to convert the values of Centigrade degrees into Fahrenheit degrees. Centigrade values are stored into a NumPy array. Go to the editor
+Sample Array [0, 12, 45.21 ,34, 99.91]
+Expected Output:
+Values in Fahrenheit degrees:
+[ 0. 12. 45.21 34. 99.91]
+Values in Centigrade degrees:
+[-17.77777778 -11.11111111 7.33888889 1.11111111 37.72777778]
+'''
+
+# c_deg = [0,12,45.21,34,99.91]
+
+# f_deg = (np.asfarray(c_deg) - 32) * 5/9
+
+# print(f_deg)
+
+'''
+15. Write a NumPy program to find the real and imaginary parts of an array of complex numbers. Go to the editor
+Expected Output:
+Original array [ 1.00000000+0.j 0.70710678+0.70710678j]
+Real part of the array:
+[ 1. 0.70710678]
+Imaginary part of the array:
+[ 0. 0.70710678]
+'''
+
